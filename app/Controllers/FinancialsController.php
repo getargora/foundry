c49647fae7c550843b65154bfbda9fc2f61445b9
Pinga <help@namingo.org>
2025-07-27 16:07:02 +0300

    Fixed issue with missing invoice

--- app/Controllers/FinancialsController.php
+++ app/Controllers/FinancialsController.php
@@ -52,6 +52,12 @@ class FinancialsController extends Controller
         $invoice_details = $db->selectRow('SELECT * FROM invoices WHERE invoice_number = ?',
         [ $invoiceNumber ]
         );
+
+        if (!$invoice_details) {
+            $this->container->get('flash')->addMessage('error', 'Invoice not found');
+            return $response->withHeader('Location', '/invoices')->withStatus(302);
+        }
+
         $billing = $db->selectRow(
             'SELECT * FROM users_contact WHERE id = ? AND type = \'billing\'',
             [ $invoice_details['billing_contact_id'] ]
@@ -156,6 +162,12 @@ class FinancialsController extends Controller
         $invoice_details = $db->selectRow('SELECT * FROM invoices WHERE invoice_number = ?',
         [ $invoiceNumber ]
         );
+        
+        if (!$invoice_details) {
+            $this->container->get('flash')->addMessage('error', 'Invoice not found');
+            return $response->withHeader('Location', '/invoices')->withStatus(302);
+        }
+
         $billing = $db->selectRow(
             'SELECT * FROM users_contact WHERE id = ? AND type = \'billing\'',
             [ $invoice_details['billing_contact_id'] ]
